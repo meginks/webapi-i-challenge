ecccf8466657164fe2c7e63fc1b66edc87a98245
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-25 15:14:21 -0500

    added express and first post request

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+
+const express = require('express'); 
+const db = require('./data/seeds/db');
+const server = express(); 
+server.use(express(json());
+
+
+server.post('/api/users'. (req, res) => {
+    const userInfo = req.body;
+
+    db.users 
+        .add(userInfo)
+        .then( user => {
+            res 
+            .status(201)
+            .json({success: true, user})
+        })
+        .catch(({ code, message }) => {
+            res 
+            .status(code)
+            .json({ 
+                success: false,
+                message
+            })
+        })
+})
